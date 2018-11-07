48d7b9fcf3f40f5bff2a8633e13757f63acca9ac
fl76 <fl76@newegg.com>
2018-11-07 16:05:34 +0800

    no message

---
+++ my-feedflow-base/src/main/java/com/beans/my/feedflow/base/App.java
@@ -0,0 +1,13 @@
+package com.beans.my.feedflow.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
